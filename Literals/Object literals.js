4d9d37dcad111fe992dba6ac4f15ca54e9ae624d
Fovir <fovir@disroot.org>
2024-09-27 17:13:24 +0800

    show simple usage of object

---
+++ Literals/Object literals.js
@@ -0,0 +1,20 @@
+// const sales = "Toyota";
+
+// function carTypes(name) {
+//     return name === "Honda" ? name : "Sorry, we don't sell ${name}.";
+// }
+
+// const car = {
+//     myCar: "Saturn",
+//     getCar: carTypes("Honda"),
+//     special: sales
+// };
+
+// console.log(car.myCar);
+// console.log(car.getCar);
+// console.log(car.special);
+
+const car = { manyCars: { a: "Saab", b: "Jeep" }, 7: "Mazda" };
+
+console.log(car.manyCars.b);
+console.log(car[7]);

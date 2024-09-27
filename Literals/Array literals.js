4645cd4bb9652956448129d42b2f776f9dd3db7e
Fovir <fovir@disroot.org>
2024-09-27 17:02:56 +0800

    show the use of array

---
+++ Literals/Array literals.js
@@ -0,0 +1,8 @@
+const coffees = ["French Roast", "Colombian", "Kona"];
+
+console.log(coffees[0]);
+console.log(coffees);
+console.log(coffees.length);
+
+for (let i = 0; i < coffees.length; i++)
+    console.log(coffees[i]);

e118cbdbaa044abb5e4a36d742cb99c2b73a0d98
Fovir <fovir@disroot.org>
2024-09-28 16:25:25 +0800

    add a negated statement example

--- Regular expressions/Matching the beginning of input using a ^ control character.js
+++ Regular expressions/Matching the beginning of input using a ^ control character.js
@@ -4,3 +4,8 @@ const fruits = ["Apple", "Watermelon", "Orange", "Avocado",
 
 const fruitsStartsWithA = fruits.filter((fruits) => /^A/.test(fruits));
 console.log(fruitsStartsWithA);
+
+const fruitsStartsWithNotA = fruits.filter((fruits) =>
+    /^[^A]/.test(fruits));
+
+console.log(fruitsStartsWithNotA);

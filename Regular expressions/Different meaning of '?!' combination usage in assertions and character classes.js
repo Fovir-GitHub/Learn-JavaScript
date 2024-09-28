39eb6104a951adeae8b3e01241f4a51e77deb845
Fovir <fovir@disroot.org>
2024-09-28 16:50:44 +0800

    show the different mean of ?! with ^

---
+++ Regular expressions/Different meaning of '?!' combination usage in assertions and character classes.js
@@ -0,0 +1,8 @@
+const orangeNotLemon =
+    "Do you want to have an orange? Yes, I do not want to have a lemon!";
+
+const selectNotLemonRegex = /[^?!]+have(?! a lemon)[^?!]+[?!]/gi;
+console.log(orangeNotLemon.match(selectNotLemonRegex));
+
+const selectNotOrangeRegex = /[^?!]+have(?! an orange)[^?!]+[?!]/gi;
+console.log(orangeNotLemon.match(selectNotOrangeRegex));

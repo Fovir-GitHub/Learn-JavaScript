97850b15d9d3bd4869c2dbcc1060a520240db29b
Fovir <fovir@disroot.org>
2024-09-28 14:50:38 +0800

    show how to use destructuring in js

---
+++ Expressions and operators/Destructuring.js
@@ -0,0 +1,11 @@
+const foo = ["one", "two", "three"];
+
+// const one = foo[0];
+// const two = foo[1];
+// const three = foo[2];
+
+// console.log(one, two, three);
+
+const [one, two, three] = foo;
+
+console.log(one, two, three);

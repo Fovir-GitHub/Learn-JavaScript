068629b775dee02db3857cdcfed4eccadcb68873
Fovir <fovir@disroot.org>
2024-09-29 10:03:55 +0800

    show how iterating undefined actions

--- Indexed collections/Iterating over arrays.js
+++ Indexed collections/Iterating over arrays.js
@@ -5,3 +5,13 @@ for (let i = 0; i < colors.length; i++)
 
 console.log();
 colors.forEach((color) => console.log(color));
+console.log();
+
+const sparseArray = ["first", "second", , "fourth"];
+sparseArray.forEach((element) => { console.log(element); });
+
+if (sparseArray[2] === undefined)
+    console.log("sparseArray[2] is undefined");
+
+const nonsparseArray = ["first", "second", undefined, "fourth"];
+nonsparseArray.forEach((element) => console.log(element));

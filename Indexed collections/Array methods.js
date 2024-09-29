1632becb61ac64cc2750fcf4ae16e88e0cb2f713
Fovir <fovir@disroot.org>
2024-09-29 10:21:09 +0800

    show some simple methods of array

---
+++ Indexed collections/Array methods.js
@@ -0,0 +1,22 @@
+// let myArray = ["1", "2", "3"];
+// myArray = myArray.concat("a", "b", "c");
+
+// console.log(myArray);
+
+// const myArray = ["Wind", "Rain", "Fire"];
+// const list = myArray.join(" - ");
+// console.log(list);
+
+// const myArray = ["1", "2"];
+// myArray.push("3");
+// console.log(myArray);
+// const last = myArray.pop();
+// console.log("last:", last);
+// console.log("myArray:", myArray);
+// const first = myArray.shift();
+// console.log(first);
+// console.log(myArray);
+
+const myArray = ["1", "2", "3"];
+myArray.unshift("4", "5");
+console.log(myArray);

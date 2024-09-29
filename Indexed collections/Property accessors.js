47ded1101ccd75d52e5dda422fd2eac340f55fd3
Fovir <fovir@disroot.org>
2024-09-29 09:43:55 +0800

    show how to access members in array

---
+++ Indexed collections/Property accessors.js
@@ -0,0 +1,12 @@
+const person1 = {};
+person1["firstname"] = "Mario";
+person1["lastname"] = "Rossi";
+
+console.log(person1.firstname);
+
+const person2 = {
+    firstname: "John",
+    lastname: "Doe",
+};
+
+console.log(person2["lastname"]);
---
+++ Indexed collections/Referring to array elements.js
@@ -0,0 +1,3 @@
+const arr = ["one", "two", "three"];
+console.log(arr[2]);
+console.log(arr["length"]);
